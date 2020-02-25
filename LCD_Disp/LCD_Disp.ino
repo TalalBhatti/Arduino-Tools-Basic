94022ebb41c0edf432aa0f11b365a2c60c57afe6
TalalBhatti <talalabhatti@gmail.com>
2020-02-25 23:16:13 +0500

    LCD

---
+++ LCD_Disp/LCD_Disp.ino
@@ -0,0 +1,26 @@
+long duration;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (9,OUTPUT);
+pinMode (10,INPUT);
+pinMode (3,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (9,HIGH);
+delayMicroseconds (10);
+digitalWrite (9,LOW);
+duration = pulseIn (10,HIGH);
+distance = duration*0.034/2;
+Serial.print(distance);
+Serial.println ("cm");
+if (distance <= 30) 
+analogWrite (3,255);
+else 
+analogWrite (3,0);
+
+}
